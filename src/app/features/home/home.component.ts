ef5e56a34aafe513c95c451ff012ca5c60bad0db
Nick Schorr <naschorr@gmail.com>
2024-01-06 14:33:41 -0800

    Added base mobile detection service
    - Added cycler to the home page that cycles between the nouns, and highlights the links

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { TextSwapperComponent } from '../../shared/text-swapper/text-swapper.component';
 import { RouterModule } from '@angular/router';
+import { IsMobileService } from '../../shared/services/is-mobile.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [TextSwapperComponent, FooterComponent, RouterModule],
+  imports: [TextSwapperComponent, FooterComponent, CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   private _actionNouns: string[];
   private _actionNoun: string;
   private readonly DEFAULT_NOUN = "stuff";
+  private readonly NOUN_CYCLE_TIME_MS = 1500;
 
   // Lifecycle
 
-  constructor() {
+  constructor(
+      private _isMobileService: IsMobileService
+  ) {
     this._actionNouns = [
       this.DEFAULT_NOUN,
       "code",
@@ -25,6 +30,17 @@ export class HomeComponent {
       "prints"
     ];
     this._actionNoun = this.DEFAULT_NOUN;
+  }
+
+  ngOnInit(): void {
+    // On mobile, cycle through the action nouns
+    if (this._isMobileService.isMobile()) {
+      let nounIndex = 1;  // Start at 1 since it's already set to the default noun
+      setInterval(() => {
+        this.actionNoun = this.actionNouns[nounIndex];
+        nounIndex = (nounIndex + 1) % this.actionNouns.length;
+      }, this.NOUN_CYCLE_TIME_MS);
+    }
   }
 
   // Properties
