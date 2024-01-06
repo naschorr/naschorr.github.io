dd859e5a7b3fade17d02ca271297c14b954a8a62
Nick Schorr <naschorr@gmail.com>
2024-01-06 00:32:47 -0800

    Simplified styling of dynamically loaded project HTML data
    - Projects.component.ts now handles checking for data updates and reapplying selected styles

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterContentChecked, ChangeDetectorRef, Component, OnInit, Renderer2 } from '@angular/core';
 import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { LogoComponent } from '../../shared/logo/logo.component';
@@ -13,20 +13,58 @@ import { ProjectLoaderService } from './services/project-loader.service';
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
-  private _projects: Project[] = [];
+export class ProjectsComponent implements OnInit, AfterContentChecked {
+  private _projects!: Project[];
+  private _projectsChanged: boolean = false;
 
   // Lifecycle
 
-  constructor(private _projectLoaderService: ProjectLoaderService) {
+  constructor(
+      private _projectLoaderService: ProjectLoaderService,
+      private _renderer: Renderer2,
+      private _changeDetectorRef: ChangeDetectorRef
+  ) { }
+
+  ngOnInit(): void {
     this._projectLoaderService.projects$.subscribe((projects: Project[]) => {
-      this._projects = projects;
+      this.projects = projects;
     });
   }
 
-  // Getters
+  ngAfterContentChecked(): void {
+    this.handleProjectsChangeUpdate();
+  }
+
+  // Getters / Setters
 
   get projects(): Project[] {
     return this._projects;
+  }
+
+  private set projects(projects: Project[]) {
+    this._projects = projects;
+    this._projectsChanged = true;
+
+    /*
+      Force a change detection cycle to eventually update the projects HTML styling. Idk it doesn't work without an
+      explicit change detection, and all of the other lifecycle methods don't pick up the changes either.
+    */
+    this._changeDetectorRef.detectChanges();
+  }
+
+  // Methods
+
+  private handleProjectsChangeUpdate(): void {
+    if (this._projectsChanged) {
+      // Apply styles to dynamically loaded HTML
+      const descriptionLinks = document.querySelectorAll(".description-container > p > a");
+      console.log(descriptionLinks);
+      Array.from(descriptionLinks).forEach((element: Element) => {
+        this._renderer.addClass(element, "bg-gradient-feature-short");
+        this._renderer.addClass(element, "clip-text");
+      });
+
+      this._projectsChanged = false;
+    }
   }
 }
