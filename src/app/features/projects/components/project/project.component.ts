0e9e92901ec18efb8f77ad3de7e3b89b0c5e658e
naschorr <naschorr@gmail.com>
2024-12-24 17:19:18 -0500

    Added randomly chosen attention grabber phrase above the repo link button

--- src/app/features/projects/components/project/project.component.ts
+++ src/app/features/projects/components/project/project.component.ts
@@ -18,11 +18,27 @@ export class ProjectComponent {
   @Input()
   public flash!: boolean;
 
+  private _repoLinkAttentionGrabbers: string[] = [
+    "Check it out:",
+    "Take a look:",
+    "Learn more:",
+    "Find out more:"
+  ];
+  private _repoLinkAttentionGrabber!: string;
+
   // Lifecycle
 
   constructor(
     private _FragmentManagerService: FragmentManagerService
-  ) {}
+  ) {
+    this._repoLinkAttentionGrabber = this._chooseRepoLinkAttentionGrabber();
+  }
+
+  // Get/Set
+
+  get repoLinkAttentionGrabber(): string {
+    return this._repoLinkAttentionGrabber;
+  }
 
   // Methods
 
@@ -33,5 +49,9 @@ export class ProjectComponent {
   public onProjectHover(): void {
     // Kill the flashing if the user hovers over the specific project, since they're already (visually) focused on it.
     this.flash = false;
+  }
+
+  private _chooseRepoLinkAttentionGrabber(): string {
+    return this._repoLinkAttentionGrabbers[Math.floor(Math.random() * this._repoLinkAttentionGrabbers.length)];
   }
 }
