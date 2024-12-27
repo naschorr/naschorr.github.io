cf2ba9a4f43c9244d5c030e741227bee3b5d2815
naschorr <naschorr@gmail.com>
2024-12-27 11:30:46 -0500

    Adding partial changes

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from '../../shared/logo/logo.component';
 import { ImageButtonComponent } from '../../shared/button/components/image-button/image-button.component';
+import { LinkFlavor } from './enums/link-flavor.enum';
 
 @Component({
   selector: 'home',
@@ -14,37 +15,42 @@ import { ImageButtonComponent } from '../../shared/button/components/image-butto
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  private _actionNouns: string[];
-  private _actionNoun: string;
-  private _actionNounCycleInterval!: ReturnType<typeof setInterval>;
-  private readonly DEFAULT_NOUN = "stuff";
+  private _taglineMap!: {};
+  private _taglineCycleInterval!: ReturnType<typeof setInterval>;
+  private readonly DEFAULT_TAGLINE = "makes stuff";
   private readonly NOUN_CYCLE_TIME_MS = 1500;
 
   // Lifecycle
 
   constructor() {
-    this._actionNouns = [
-      this.DEFAULT_NOUN,
-      "code",
-      "projects",
-      "prints"
-    ];
-    this._actionNoun = this.DEFAULT_NOUN;
+    this._taglineMap = {
+      [LinkFlavor.CODE]: [
+        "writes code",
+        "designs tools",
+        "writes docs"
+      ],
+      [LinkFlavor.PORTFOLIO]: [
+        "designs projects",
+        "solves problems",
+        "builds solutions"
+      ],
+      [LinkFlavor.PRINT]: [
+        "makes prints",
+        "creates models",
+        "designs devices"
+      ]
+    }
   }
 
   ngOnInit(): void {
-    let nounIndex = 1;  // Start at 1 since it's already set to the default noun
-    this._actionNounCycleInterval = setInterval(() => {
+    let taglineIndex = 1;  // Start at 1 since it's already set to the default tagline
+    this._taglineCycleInterval = setInterval(() => {
       this.actionNoun = this.actionNouns[nounIndex];
       nounIndex = (nounIndex + 1) % this.actionNouns.length;
     }, this.NOUN_CYCLE_TIME_MS);
   }
 
   // Properties
-
-  get actionNouns(): string[] {
-    return this._actionNouns;
-  }
 
   get actionNoun(): string {
     return this._actionNoun;
@@ -58,11 +64,31 @@ export class HomeComponent implements OnInit {
 
   // Methods
 
-  resetActionNoun() {
+  getTagline(index: number): string {
+    const keys = Object.keys(this._taglineMap);
+    if (index % keys.length + 1 === 0) {
+      return this.DEFAULT_TAGLINE;
+    }
+
+    const linkFlavor = keys[index % keys.length];
+    const taglineIndex = Math.floor(index / keys.length);
+
+    return this._taglineMap[linkFlavor][taglineIndex];
+
+    count %= this._taglineMap[flavor].length + 1;
+
+    if (count === 0) {
+      return this.DEFAULT_TAGLINE;
+    }
+
+    return this._taglineMap[flavor][count];
+  }
+
+  resetTagline() {
     this.actionNoun = this.DEFAULT_NOUN;
   }
 
   stopActionNounCycling() {
-    clearInterval(this._actionNounCycleInterval);
+    clearInterval(this._taglineCycleInterval);
   }
 }
