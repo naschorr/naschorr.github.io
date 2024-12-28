c1c55b4f2ebc982b64867150ce1d5ef5a87c84e2
Nick Schorr <naschorr@gmail.com>
2024-12-28 14:34:52 -0800

    Updated `text-swapper` to try and keep it's contents centered if given enough space
    - Minor homepage formatting to improve layout

--- src/app/shared/text-swapper/text-swapper.component.ts
+++ src/app/shared/text-swapper/text-swapper.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, ChangeDetectorRef, Component, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, Input, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'text-swapper',
@@ -14,29 +14,48 @@ export class TextSwapperComponent implements AfterViewInit {
   @Input()
   public activeOption: string = "";
 
+  private _maxWidthPx: number = 0;
   private _changeDetectorRef: ChangeDetectorRef;
-  private _widthPixels: number = 150; // Guess to get started
+  private _renderer: Renderer2;
 
   // Lifecycle
 
-  constructor(changeDetectorRef: ChangeDetectorRef) {
+  constructor(
+      changeDetectorRef: ChangeDetectorRef,
+      renderer: Renderer2
+  ) {
     this._changeDetectorRef = changeDetectorRef;
+    this._renderer = renderer;
   }
 
-  ngAfterViewInit() {
-    let activeOption = document.querySelector<HTMLElement>(".swap-option");
-    this._widthPixels = activeOption?.offsetWidth ?? this._widthPixels;
+  ngAfterViewInit(): void {
+    // Get all of the options
+    const swapOptionElements: HTMLElement[] = Array.from(document.querySelectorAll(".swap-option"));
 
-    /*
-      Force a change detection here to ensure the _widthPixels is updated in the template. Otherwise there's a judder as
-      it updates after some interactions (ex mouseover)
-    */
+    // Find the largest option
+    let maxWidthPx = 0;
+    swapOptionElements.forEach(element => {
+      const width = element.offsetWidth;
+      maxWidthPx = width > maxWidthPx ? width : maxWidthPx;
+    });
+    this.maxWidthPx = maxWidthPx;
+
+    // Set the width of all options to the largest width
+    swapOptionElements.forEach(element => {
+      this._renderer.setStyle(element, "width", `${this.maxWidthPx}px`);
+    });
+
+    // Force angular to detect this change
     this._changeDetectorRef.detectChanges();
   }
 
   // Properties
 
-  get widthPixels(): number {
-    return this._widthPixels;
+  get maxWidthPx(): number {
+    return this._maxWidthPx;
+  }
+
+  set maxWidthPx(value: number) {
+    this._maxWidthPx = value;
   }
 }
