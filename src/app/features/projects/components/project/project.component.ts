3ba16f2a1dd4347056732158b9a76b8f2b7c9248
naschorr <naschorr@gmail.com>
2025-12-28 02:44:06 -0500

    Added ability to filter by technology in the project panel

--- src/app/features/projects/components/project/project.component.ts
+++ src/app/features/projects/components/project/project.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { GalleryModule } from '../../../../shared/gallery/gallery.module';
 import { FragmentManagerService } from '../../../../shared/services/fragment-manager.service';
 import { ImageButtonComponent } from '../../../../shared/button/components/image-button/image-button.component';
+import { ProjectFilterService } from '../../services/project-filter.service';
 
 @Component({
     selector: 'project',
@@ -28,7 +29,8 @@ export class ProjectComponent {
   // Lifecycle
 
   constructor(
-    private _FragmentManagerService: FragmentManagerService
+    private _fragmentManagerService: FragmentManagerService,
+    private _projectFilterService: ProjectFilterService
   ) {
     this._repoLinkAttentionGrabber = this._chooseRepoLinkAttentionGrabber();
   }
@@ -42,12 +44,19 @@ export class ProjectComponent {
   // Methods
 
   public onProjectClick(): void {
-    this._FragmentManagerService.setFragment(this.project.id);
+    this._fragmentManagerService.setFragment(this.project.id);
   }
 
   public onProjectHover(): void {
     // Kill the flashing if the user hovers over the specific project, since they're already (visually) focused on it.
     this.flash = false;
+  }
+
+  public onFilterHover(category: string, name: string): void {
+    const propertyFilter = this._projectFilterService.getPropertyFilterByCategoryName(category, name);
+    if (propertyFilter) {
+      this._projectFilterService.onToggleFilter(propertyFilter);
+    }
   }
 
   private _chooseRepoLinkAttentionGrabber(): string {
