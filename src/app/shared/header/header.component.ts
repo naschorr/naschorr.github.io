d15c77a954469d9422366cf7131fb7adf0e39b45
naschorr <naschorr@gmail.com>
2025-12-29 00:59:37 -0500

    Updated header to use `ImageButtons`

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { LogoComponent } from '../logo/logo.component';
-
 import { RouterModule } from '@angular/router';
+import { LogoComponent } from '../logo/logo.component';
+import { ImageButtonComponent } from '../button/components/image-button/image-button.component';
 
 @Component({
     selector: 'header',
-    imports: [RouterModule, LogoComponent],
+    imports: [RouterModule, LogoComponent, ImageButtonComponent],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
