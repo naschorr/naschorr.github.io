fa29f819cf57e0c515868df953c8a1c0a0951c87
Nick Schorr <naschorr@gmail.com>
2026-03-28 11:23:51 -0700

    Upgrade from angular 20 to 21

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { APP_CONFIG } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, APP_CONFIG)
+bootstrapApplication(AppComponent, {...APP_CONFIG, providers: [provideZoneChangeDetection(), ...APP_CONFIG.providers]})
   .catch((err) => console.error(err));
